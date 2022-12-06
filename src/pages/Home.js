56f62415dfe5237f3b7292d4ec5d9b670928d111
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2022-12-07 00:55:06 +0100

    Creo proyecto

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='mb-20'>
+        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>Hello, Welcome to my Blog</h1>
+        <p className='mx-auto leading-relaxed text-base mb-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed consectetur nisl. Nullam faucibus laoreet sapien, sed vehicula eros dignissim finibus. Nam tincidunt ipsum et est efficitur faucibus. Maecenas odio dolor, mollis malesuada nulla eu, aliquam dictum magna. Sed vehicula metus eget sagittis tempor.</p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed consectetur nisl. Nullam faucibus laoreet sapien, sed vehicula eros dignissim finibus. Nam tincidunt ipsum et est efficitur faucibus. Maecenas odio dolor, mollis malesuada nulla eu, aliquam dictum magna. Sed vehicula metus eget sagittis tempor.</p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed consectetur nisl. Nullam faucibus laoreet sapien, sed vehicula eros dignissim finibus. Nam tincidunt ipsum et est efficitur faucibus. Maecenas odio dolor, mollis malesuada nulla eu, aliquam dictum magna. Sed vehicula metus eget sagittis tempor.</p>
+    </div>
+    
+  )
+}
+
+export default Home
