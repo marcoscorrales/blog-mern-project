623f10c93338ad544642597d9a3ec89adf5ad11f
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2022-12-07 19:36:21 +0100

    Añado paginas de blog y contenido

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 //Pages
+import About from "./pages/About";
+import Article from "./pages/Article";
+import ArticlesList from "./pages/ArticlesList";
 import Home from "./pages/Home";
+
+//Components
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar/>
       <div className="max-w-screen-md mx-auto pt-20">
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/articles-list" element={<ArticlesList/>}/>
+          <Route path="/article/:name" element={<Article/>}/>
         </Routes>
       </div>
     </Router>
