d79bd26495e227085db8f05b93e0e4e5754b28b8
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2022-12-11 20:33:34 +0100

    Añado componente de comentarios

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import articleContent from "./Article-content";
 
@@ -7,10 +7,24 @@ import NotFound from "./NotFound";
 
 //Components
 import Articles from "../components/Articles";
+import CommentsList from "../components/CommentsList";
+import AddCommentForm from "../components/AddCommentForm";
 
 const Article = () => {
   const { name } = useParams();
   const article = articleContent.find((article) => article.name === name);
+  const [articleInfo, setArticleInfo] = useState({ comments: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(`/api/articles/${name}`);
+      const body = await result.json();
+      console.log(body);
+      setArticleInfo(body);
+    }
+    fetchData();
+  }, [name]);
+
   if (!article) return <NotFound/>;
   const otherArticles = articleContent.filter(article => article.name !== name)
   return (
@@ -23,6 +37,8 @@ const Article = () => {
           {paragraph}
         </p>
       ))}
+      <CommentsList comments={articleInfo.comments}/>
+      <AddCommentForm articleName={name} setArticleInfo={setArticleInfo}/>
       <h1 className="sm:text-2xl text-xl font-bold my-4 text-gray-900">Other Articles</h1>
       <div className="flex flex-wrap -m-4">
         <Articles articles={otherArticles}/>
